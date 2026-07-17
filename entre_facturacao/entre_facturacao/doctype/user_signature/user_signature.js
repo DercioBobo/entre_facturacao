4cae4870fed1be207d929fd5085a5b164cabe665
Dércio Bobo <38473260+DercioBobo@users.noreply.github.com>
2026-07-17 14:33:44 +0200

    Filter link on user signature

---
+++ entre_facturacao/entre_facturacao/doctype/user_signature/user_signature.js
@@ -0,0 +1,16 @@
+frappe.ui.form.on("User Signature", {
+	onload(frm) {
+		frm.set_query("user", () => {
+			if (frappe.user.has_role("System Manager")) {
+				return {};
+			}
+			return {
+				filters: { name: frappe.session.user },
+			};
+		});
+
+		if (frm.is_new() && !frm.doc.user && !frappe.user.has_role("System Manager")) {
+			frm.set_value("user", frappe.session.user);
+		}
+	},
+});
