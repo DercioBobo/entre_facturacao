b4e3e0460c78485ebcba9cf3cf64837efb59411a
Dércio Bobo <38473260+DercioBobo@users.noreply.github.com>
2026-07-27 13:38:35 +0200

    preview of image

--- entre_facturacao/entre_facturacao/doctype/user_signature/user_signature.js
+++ entre_facturacao/entre_facturacao/doctype/user_signature/user_signature.js
@@ -1,3 +1,24 @@
+function render_signature_preview(frm) {
+	const $wrapper = frm.get_field("signature_preview").$wrapper;
+	$wrapper.empty();
+
+	if (!frm.doc.signature_image) {
+		$wrapper.html(`<div class="text-muted">${__("Upload a signature image to see a preview.")}</div>`);
+		return;
+	}
+
+	const width = frm.doc.image_width || 150;
+	const height = frm.doc.image_height || 60;
+	const src = frappe.utils.escape_html(frm.doc.signature_image);
+
+	$wrapper.html(`
+		<div style="display:inline-block; padding:10px; border:1px dashed var(--border-color);">
+			<img src="${src}" style="width:${width}px; height:${height}px; object-fit:contain;">
+		</div>
+		<div class="text-muted small" style="margin-top:4px;">${width}px &times; ${height}px</div>
+	`);
+}
+
 frappe.ui.form.on("User Signature", {
 	onload(frm) {
 		frm.set_query("user", () => {
@@ -18,5 +39,21 @@ frappe.ui.form.on("User Signature", {
 				__("Save first, then upload the signature image.")
 			);
 		}
+	},
+
+	refresh(frm) {
+		render_signature_preview(frm);
+	},
+
+	signature_image(frm) {
+		render_signature_preview(frm);
+	},
+
+	image_width(frm) {
+		render_signature_preview(frm);
+	},
+
+	image_height(frm) {
+		render_signature_preview(frm);
 	},
 });
